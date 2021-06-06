2bbeb575b0ab8af0cc520b11b4d8a3b75117fad0
Benedict Hazel <bwhazel@gmail.com>
2021-06-06 23:07:15 +0100

    Add ASP.NET MVC web site project.

---
+++ BWHazel.Aka.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BWHazel.Aka.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

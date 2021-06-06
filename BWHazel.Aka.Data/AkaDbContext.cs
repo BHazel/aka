7cfde09c73821f4014bf5934a296507e068091e8
Benedict Hazel <bwhazel@gmail.com>
2021-06-06 22:57:04 +0100

    Add data project with database context.

---
+++ BWHazel.Aka.Data/AkaDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BWHazel.Aka.Data
+{
+    public class AkaDbContext : DbContext
+    {
+        /// <summary>
+        /// Initialise a new instance of the <see cref="AkaDbContext"/> class.
+        /// </summary>
+        /// <param name="options">The database context options.</param>
+        public AkaDbContext(DbContextOptions<AkaDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}

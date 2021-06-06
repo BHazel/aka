696b3546638c5cf53bc101bb4476f9c155b46e96
Benedict Hazel <bwhazel@gmail.com>
2021-06-06 22:43:25 +0100

    Add model project with short URL data model.

---
+++ BWHazel.Aka.Model/ShortUrl.cs
@@ -0,0 +1,23 @@
+﻿namespace BWHazel.Aka.Model
+{
+    /// <summary>
+    /// Represents a short URL.
+    /// </summary>
+    public class ShortUrl
+    {
+        /// <summary>
+        /// Gets or sets the short URL ID.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the URL.
+        /// </summary>
+        public string Url { get; set; }
+    }
+}

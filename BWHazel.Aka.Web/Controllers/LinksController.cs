959a36858451514e26e81e34b955a88353669003
Benedict Hazel <bwhazel@gmail.com>
2021-06-09 19:56:47 +0100

    Add Links controller and index view and remove privacy policy references.

---
+++ BWHazel.Aka.Web/Controllers/LinksController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BWHazel.Aka.Web.Controllers
+{
+    /// <summary>
+    /// The links controller.
+    /// </summary>
+    public class LinksController : Controller
+    {
+        private readonly ILogger<LinksController> logger;
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="LinksController"/> class.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
+        public LinksController(ILogger<LinksController> logger)
+        {
+            this.logger = logger;
+        }
+
+        /// <summary>
+        /// Returns the links index view.
+        /// </summary>
+        /// <returns>The links index view.</returns>
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}

a98f6565c47205114cc45663c7f0ac2340e08ea6
Benedict Hazel <bwhazel@gmail.com>
2021-07-08 21:34:50 +0100

    Add Key Vault configuration support on startup.

--- BWHazel.Aka.Web/Program.cs
+++ BWHazel.Aka.Web/Program.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Azure.Identity;
 
 namespace BWHazel.Aka.Web
 {
@@ -8,6 +11,11 @@ namespace BWHazel.Aka.Web
     /// </summary>
     public static class Program
     {
+        private const string AzureAdTenantIdKey = "AzureAD:TenantId";
+        private const string AzureAdClientIdKey = "AzureAD:ClientId";
+        private const string AzureAdClientSecretKet = "AzureAD:ClientSecret";
+        private const string SecretsKeyVaultKey = "Secrets:KeyVault";
+
         /// <summary>
         /// Program entry point.
         /// </summary>
@@ -26,6 +34,17 @@ namespace BWHazel.Aka.Web
         /// <returns>The host builder.</returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    IConfigurationRoot rootConfig = config.Build();
+                    config.AddAzureKeyVault(
+                        new Uri($"https://{rootConfig[SecretsKeyVaultKey]}.vault.azure.net"),
+                        new ClientSecretCredential(
+                            rootConfig[AzureAdTenantIdKey],
+                            rootConfig[AzureAdClientIdKey],
+                            rootConfig[AzureAdClientSecretKet])
+                        );
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
